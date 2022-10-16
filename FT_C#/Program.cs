b7b778b6d0c1b90231037ede4613705b97db62dd
Mariia <MK193@yandex.ru>
2022-10-16 13:38:45 +0300

    Added program

---
+++ FT_C#/Program.cs
@@ -0,0 +1,21 @@
+﻿//Из заданного массива строк сформировать массив  строк, длина элементов которого менее или равно 3.
+
+string[] mass = { "Monday", "3554", "hi", "next", "123", "lol", "<3", "нет"};
+
+string[] ThreeMass(string[] mass)
+{
+    string[] new_mas = new string[1];
+    int j = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i].Length <= 3)
+        {
+            Array.Resize(ref new_mas, 1 + j);
+            new_mas[j] = mass[i];
+            j++;
+        }
+    }
+    return new_mas;
+}
+Console.WriteLine("Массив, длина элементов которого меньше или равна 3 -");
+Console.WriteLine(String.Join("|", ThreeMass(mass)));
